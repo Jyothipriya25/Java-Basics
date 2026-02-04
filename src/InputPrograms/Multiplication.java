2a1ab76d3e9a60f13c09bfb79a5831522ec7b8b5
Jyothipriya25 <jyothipriyama@gmail.com>
2026-02-04 14:08:51 +0530

    Add Multiplication program to calculate product

---
+++ src/InputPrograms/Multiplication.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Product: " + (a * b));
+
+        sc.close();
+    }
+}
